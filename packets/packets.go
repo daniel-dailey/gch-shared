389202a93e2cedef83eea01c2ee2fd7582376dd1
Daniel Dailey <daniel.dailey@acres4.com>
2023-11-04 20:48:19 -0700

    add comm packet

--- packets/packets.go
+++ packets/packets.go
@@ -1,5 +1,20 @@
 package packets
 
+import "github.com/daniel-dailey/gch-shared/entities"
+
+type CommPacketType int
+
+const (
+	CommPacketTypeMessage CommPacketType = iota
+	CommPacketTypeUserList
+)
+
+type CommPacket struct {
+	MessageType   CommPacketType
+	MessagePacket *MessagePacket
+	UserList      map[string]*entities.User
+}
+
 type MessagePacket struct {
 	UUID      string
 	UserUUID  string
@@ -16,4 +31,17 @@ type MessageContent struct {
 type UUIDNamePacket struct {
 	UUID string
 	Name string
+}
+
+func BuildCommPacket(typ CommPacketType, payload interface{}) *CommPacket {
+	cp := &CommPacket{
+		MessageType: typ,
+	}
+	switch typ {
+	case CommPacketTypeMessage:
+		cp.MessagePacket = payload.(*MessagePacket)
+	case CommPacketTypeUserList:
+		cp.UserList = payload.(map[string]*entities.User)
+	}
+	return cp
 }

dbef2cc74c92d84aa01d6d1fe068e2abeb747fec
Daniel Dailey <daniel.dailey@acres4.com>
2023-11-12 20:59:42 -0800

    latest

--- entities/entities.go
+++ entities/entities.go
@@ -1,8 +1,33 @@
 package entities
 
 import (
+	"github.com/daniel-dailey/gch-shared/packets"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+type TaskState int
+
+const (
+	TaskStateIdle TaskState = iota
+	TaskStateInProgress
+	TaskStateDone
+	TaskStateParked
+)
+
+func (ts TaskState) String() string {
+	switch ts {
+	case TaskStateIdle:
+		return "Idle"
+	case TaskStateInProgress:
+		return "In Progress"
+	case TaskStateDone:
+		return "Done"
+	case TaskStateParked:
+		return "Parked"
+	default:
+		return ""
+	}
+}
 
 type User struct {
 	ID       primitive.ObjectID `bson:"_id"`
@@ -19,13 +44,15 @@ type Story struct {
 }
 
 type Task struct {
-	ID            primitive.ObjectID `bson:"_id"`
-	Title         string             `bson:"title"`
-	Description   string             `bson:"description"`
-	Assignees     []string           `bson:"assignees"`
-	PriorityLevel int                `bson:"priority"`
-	CreatedAt     int64              `bson:"created_at"`
-	Modified      int64              `bson:"modified_at"`
+	ID            primitive.ObjectID       `bson:"_id"`
+	Title         string                   `bson:"title"`
+	Description   string                   `bson:"description"`
+	Assignees     []string                 `bson:"assignees"`
+	PriorityLevel int                      `bson:"priority"`
+	CreatedAt     int64                    `bson:"created_at"`
+	Modified      int64                    `bson:"modified_at"`
+	Status        TaskState                `bson:"state"`
+	Comments      []*packets.MessagePacket `bson:"comments"`
 }
 
 func MakeTask(title, description string, assignees []string) *Task {
@@ -33,6 +60,7 @@ func MakeTask(title, description string, assignees []string) *Task {
 		Title:       title,
 		Description: description,
 		Assignees:   make([]string, 0),
+		Comments:    make([]*packets.MessagePacket, 0),
 	}
 	return t
 }
